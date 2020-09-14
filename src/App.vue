cf364d781fc5bb90866732a2e1ad91430a928da2
reubenwedson <reubenwedson@gmail.com>
2020-09-14 15:49:58 +0300

    Adding header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    background-color: rgb(234, 237, 237);
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
+<script>
+  import Header from "./components/navigation/Header";
+  export default {
+    components: {Header}
+  }
+</script>
---
+++ src/components/navigation/Header.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="header">
+        <img
+                class="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="header__logo"
+        />
+        <div class="header__search">
+            <label>
+                <input class="header__searchInput" type="text" />
+            </label>
+        </div>
+        <div class="header__nav">
+            <div class="header__option">
+                <span class="header__optionLineOne">Hello Guest</span>
+                <span class="header__optionLineTwo">Sign In</span>
+            </div>
+            <div class="header__option">
+                <span class="header__optionLineOne">Returns</span>
+                <span class="header__optionLineTwo">& Orders</span>
+            </div>
+            <div class="header__option">
+                <span class="header__optionLineOne">Your</span>
+                <span class="header__optionLineTwo">Prime</span>
+            </div>
+            <div class="header__optionBasket">
+                <span class="header__optionLineTwo header__basketCount">0</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+        height: 60px;
+        display: flex;
+        align-items: center;
+        background-color: #131921;
+        position: sticky;
+        top: 0;
+        z-index: 100;
+    }
+
+    .header__logo {
+        width: 100px;
+        object-fit: contain;
+        margin: 18px 20px 0;
+    }
+
+    .header__search {
+        display: flex;
+        flex: 1;
+        align-items: center;
+        border-radius: 24px;
+    }
+
+    .header__searchInput {
+        height: 12px;
+        padding: 10px;
+        border: none;
+        width: 100%;
+    }
+
+    .header__searchIcon {
+        padding: 5px;
+        height: 22px !important;
+        background-color: #cd9042;
+
+    }
+
+    .header__nav {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .header__option {
+        display: flex;
+        flex-direction: column;
+        margin: 0 10px;
+        color: #ffffff;
+    }
+
+    .header__optionLineOne {
+        font-size: 10px;
+    }
+
+    .header__optionLineTwo {
+        font-size: 13px;
+        font-weight: 800;
+    }
+
+    .header__optionBasket {
+        display: flex;
+        align-items: center;
+        color: #ffffff;
+    }
+
+    .header__basketCount {
+        margin: 0 10px;
+    }
+</style>
