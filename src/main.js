aa9c4289251991c641ce5f9c78186658fda3275b
reubenwedson <reubenwedson@gmail.com>
2020-09-21 15:03:24 +0300

    Fixing code quality

--- src/main.js
+++ src/main.js
@@ -3,17 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(
-    faSearch,
-    faShoppingCart
-)
+library.add(faSearch, faShoppingCart);
 
 Vue.config.productionTip = false;
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
   router,
