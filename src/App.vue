33efa67c33a666f46e9c80b5814d539ed305abaa
reubenwedson <reubenwedson@gmail.com>
2020-09-21 12:32:36 +0300

    no change

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,24 @@
 </template>
 
 <script>
-  import Header from "./components/navigation/Header";
-  export default {
-    components: {Header}
-  }
+import Header from "./components/navigation/Header";
+export default {
+  components: { Header }
+};
 </script>
 
 <style>
-  * {
-    margin: 0;
-  }
+* {
+  margin: 0;
+}
 
-  #app {
-    background-color: rgb(234, 237, 237);
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+#app {
+  background-color: rgb(234, 237, 237);
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
--- src/components/navigation/Header.vue
+++ src/components/navigation/Header.vue
@@ -1,109 +1,108 @@
 <template>
-    <div class="header">
-        <router-link to="/">
-        <img
-                class="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="header__logo"
-        />
-        </router-link>
-        <div class="header__search">
-            <input class="header__searchInput" type="text" />
-            <font-awesome-icon class="header__searchIcon" icon="search" />
-        </div>
-        <div class="header__nav">
-            <div class="header__option">
-                <span class="header__optionLineOne">Hello Guest</span>
-                <span class="header__optionLineTwo">Sign In</span>
-            </div>
-            <div class="header__option">
-                <span class="header__optionLineOne">Returns</span>
-                <span class="header__optionLineTwo">& Orders</span>
-            </div>
-            <div class="header__option">
-                <span class="header__optionLineOne">Your</span>
-                <span class="header__optionLineTwo">Prime</span>
-            </div>
-            <div class="header__optionBasket">
-                <font-awesome-icon icon="shopping-cart" />
-                <span class="header__optionLineTwo header__basketCount">0</span>
-            </div>
-        </div>
+  <div class="header">
+    <router-link to="/">
+      <img
+        class="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="header__logo"
+      />
+    </router-link>
+    <div class="header__search">
+      <input class="header__searchInput" type="text" />
+      <font-awesome-icon class="header__searchIcon" icon="search" />
     </div>
+    <div class="header__nav">
+      <div class="header__option">
+        <span class="header__optionLineOne">Hello Guest</span>
+        <span class="header__optionLineTwo">Sign In</span>
+      </div>
+      <div class="header__option">
+        <span class="header__optionLineOne">Returns</span>
+        <span class="header__optionLineTwo">& Orders</span>
+      </div>
+      <div class="header__option">
+        <span class="header__optionLineOne">Your</span>
+        <span class="header__optionLineTwo">Prime</span>
+      </div>
+      <div class="header__optionBasket">
+        <font-awesome-icon icon="shopping-cart" />
+        <span class="header__optionLineTwo header__basketCount">0</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+export default {
+  name: "Header"
+};
 </script>
 
 <style scoped>
-    .header {
-        height: 60px;
-        display: flex;
-        align-items: center;
-        background-color: #131921;
-        position: sticky;
-        top: 0;
-        z-index: 100;
-    }
+.header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  background-color: #131921;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+}
 
-    .header__logo {
-        width: 100px;
-        object-fit: contain;
-        margin: 18px 20px 0;
-    }
+.header__logo {
+  width: 100px;
+  object-fit: contain;
+  margin: 18px 20px 0;
+}
 
-    .header__search {
-        display: flex;
-        flex: 1;
-        align-items: center;
-        border-radius: 24px;
-    }
+.header__search {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  border-radius: 24px;
+}
 
-    .header__searchInput {
-        height: 12px;
-        padding: 10px;
-        border: none;
-        width: 100%;
-    }
+.header__searchInput {
+  height: 12px;
+  padding: 10px;
+  border: none;
+  width: 100%;
+}
 
-    .header__searchIcon {
-        padding: 5px;
-        height: 22px !important;
-        background-color: #cd9042;
+.header__searchIcon {
+  padding: 5px;
+  height: 22px !important;
+  background-color: #cd9042;
+}
 
-    }
+.header__nav {
+  display: flex;
+  justify-content: space-evenly;
+}
 
-    .header__nav {
-        display: flex;
-        justify-content: space-evenly;
-    }
+.header__option {
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  color: #ffffff;
+}
 
-    .header__option {
-        display: flex;
-        flex-direction: column;
-        margin: 0 10px;
-        color: #ffffff;
-    }
+.header__optionLineOne {
+  font-size: 10px;
+}
 
-    .header__optionLineOne {
-        font-size: 10px;
-    }
+.header__optionLineTwo {
+  font-size: 13px;
+  font-weight: 800;
+}
 
-    .header__optionLineTwo {
-        font-size: 13px;
-        font-weight: 800;
-    }
+.header__optionBasket {
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+}
 
-    .header__optionBasket {
-        display: flex;
-        align-items: center;
-        color: #ffffff;
-    }
-
-    .header__basketCount {
-        margin: 0 10px;
-    }
+.header__basketCount {
+  margin: 0 10px;
+}
 </style>
